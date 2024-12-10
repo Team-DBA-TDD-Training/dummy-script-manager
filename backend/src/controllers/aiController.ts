0ca8d49b3001920b58c46bb7065e68f914d21112
deniz <deniz.kartal@studio3t.com>
2024-12-10 16:50:31 +0100

    manual deploy to aws

--- backend/src/controllers/aiController.ts
+++ backend/src/controllers/aiController.ts
@@ -1,30 +1,35 @@
-import { OpenAI } from 'openai';
+import { OpenAI } from "openai";
 import { Request, Response, NextFunction } from "express";
 
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_KEY
+  apiKey: process.env.OPENAI_KEY || "",
 });
 
-export const generateScript = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-  const { message } = req.body
+export const generateScript = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  const { message } = req.body;
   try {
     const response = await openai.chat.completions.create({
-      model: 'gpt-3.5-turbo',
+      model: "gpt-3.5-turbo",
       messages: [
         {
-          role: 'system',
-          content: 'You are a helpful assistant that generates MongoDB scripts based on user prompts and return only json with code field',
+          role: "system",
+          content:
+            "You are a helpful assistant that generates MongoDB scripts based on user prompts and return only json with code field",
         },
         {
-          role: 'user',
+          role: "user",
           content: message,
         },
       ],
     });
     const code = JSON.parse(response.choices[0].message.content ?? "{}");
 
-    res.status(201).json(code)
+    res.status(201).json(code);
   } catch (error) {
-    throw error
+    throw error;
   }
-}
+};
