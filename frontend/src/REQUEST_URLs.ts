65a8a112b23a13edbc848dad74de36502119e85f
deniz <deniz.kartal@studio3t.com>
2024-12-11 09:03:10 +0100

    get rid of hard coded url

--- frontend/src/REQUEST_URLs.ts
+++ frontend/src/REQUEST_URLs.ts
@@ -1,10 +1,11 @@
-export const API_BASE_URL = "http://localhost:5100/api/scripts";
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
 export const FETCH_ALL_SCRIPTS_API_URL = API_BASE_URL + "/";
 
 export const DELETE_SCRIPTS_API_URL = API_BASE_URL + "/";
-export const CREATE_SCRIPTS_API_URL = API_BASE_URL + "/"
-export const UPDATE_SCRIPTS_API_URL = API_BASE_URL + "/"
+export const CREATE_SCRIPTS_API_URL = API_BASE_URL + "/";
+export const UPDATE_SCRIPTS_API_URL = API_BASE_URL + "/";
 
-export const MARK_FAVORITE_API_URL = API_BASE_URL + "/markFavorite/"
+export const MARK_FAVORITE_API_URL = API_BASE_URL + "/markFavorite/";
 
-export const UNMARK_FAVORITE_API_URL = API_BASE_URL + "/unMarkFavorite/"
+export const UNMARK_FAVORITE_API_URL = API_BASE_URL + "/unMarkFavorite/";
