c3f7ac6e76c32b52f20c76f49a5d45a2143a6813
dkartal <deniz.kartal@3tsoftwarelabs.com>
2025-01-09 15:37:52 +0100

    introducing system test

---
+++ system-test/acceptance/acceptance.test.ts
@@ -0,0 +1,5 @@
+describe("Acceptance Tests Placeholder", () => {
+  it("should pass a basic placeholder acceptance test", () => {
+    expect(1).toBe(1);
+  });
+});
---
+++ system-test/e2e/e2e.test.ts
@@ -0,0 +1,5 @@
+describe("E2E Tests Placeholder", () => {
+  it("should pass a basic placeholder e2e test", () => {
+    expect(1).toBe(1);
+  });
+});
