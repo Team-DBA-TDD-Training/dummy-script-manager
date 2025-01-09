353abf69a90de69088dc63c50f4b9a6f05239be2
Erdenezul Batmunkh <erdenezul.batmunkh@3tsoftwarelabs.com>
2025-01-09 17:01:28 +0100

    Restore cypress config

--- system-test/cypress/e2e/front-end/main.cy.js
+++ system-test/cypress/e2e/front-end/main.cy.js
@@ -1,5 +1,5 @@
 
-const FRONT_URL = 'http://localhost:5173';
+const FRONT_URL = 'http://tdd-team-training.s3-website-us-east-1.amazonaws.com/';
 
 describe('Front-end spec', () => {
   it('Basic health check', () => {
