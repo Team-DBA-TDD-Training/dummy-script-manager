f55531e0ce4f181202ec645e943a16aa9b574ed3
Erdenezul Batmunkh <erdenezul.batmunkh@3tsoftwarelabs.com>
2025-01-10 15:38:41 +0100

    Use base url from env

--- system-test/cypress.config.js
+++ system-test/cypress.config.js
@@ -1,8 +1,23 @@
 const { defineConfig } = require("cypress");
 
+/**
+ * @behavior We are gonna pass NODE_ENV from pipeline to run on different
+ * environment.
+ */
+const getBaseUrl = () => {
+  switch(process.env.NODE_ENV) {
+    case 'production':
+      return "http://prod_url";
+    case 'uat':
+      return 'http://script-manager-backend-uat.us-east-1.elasticbeanstalk.com/';
+    default:
+      return 'http://localhost:3000'
+  }
+}
+
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000',
+    baseUrl: getBaseUrl(),
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
