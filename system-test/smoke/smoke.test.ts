a85d71b74b55349f5934b8de628978698c6473b7
Anam Sadiq <anam.sadiq@3tsoftwarelabs.com>
2025-01-13 16:04:36 +0100

    #114: adds smoke tests for health check (sub-ticket: #115)

--- system-test/smoke/smoke.test.ts
+++ system-test/smoke/smoke.test.ts
@@ -1,5 +1,24 @@
-describe("Smoke Tests Placeholder", () => {
-  it("should pass a basic smoke integration test", () => {
-    expect(1).toBe(1);
+const frisby = require('frisby');
+const Joi = frisby.Joi;
+export const REST_API_BASE_URL = import.meta.env.REST_API_BASE_URL;
+export const SCRIPT_API_EXT = '/api/scripts';
+
+describe("Tests health checks", () => {
+  it("if backend server is up and running", async () => {
+    return frisby
+      .get(REST_API_BASE_URL)
+      .expect('status', 200)
+  });
+
+  it("if GET API returns the required fields ", async () => {
+    return frisby
+      .get(REST_API_BASE_URL + SCRIPT_API_EXT)
+      .expect('status', 200)
+      .expect('jsonTypes', '*', { // Assert *each* object in array
+        '_id': Joi.string().required(),
+        'title': Joi.string().required(),
+        'code': Joi.string().required(),
+        'lastUpdatedAt': Joi.date().iso().required(),
+      });
   });
 });
