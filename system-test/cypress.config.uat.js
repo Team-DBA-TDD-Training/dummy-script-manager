d970341a326c5a7dfc5345e73eb5631c29f45718
erdenezul <erdenezulb@gmail.com>
2025-01-22 12:48:53 +0100

    exclude stub tests from uat without stub

--- system-test/cypress.config.uat.js
+++ system-test/cypress.config.uat.js
@@ -4,11 +4,15 @@ module.exports = defineConfig({
   NODE_ENV: process.env.NODE_ENV || 'development', // Default to 'development'
   e2e: {
     baseUrl: 'http://script-manager-frontend-uat.us-east-1.elasticbeanstalk.com/',
+    /**
+     * Since we are running against actual environment without stub, tests with
+     * stub should be excluded
+     */
+    excludeSpecPattern: [
+      'acceptance/ai.cy.js'
+    ],
     specPattern: [
       'acceptance/*.cy.{js,jsx,ts,tsx}'
     ],
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
   },
 });
