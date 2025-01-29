ebeefde1552383aaa9063446d7617c95674ca44f
Anam Sadiq <anam.sadiq@3tsoftwarelabs.com>
2025-01-29 14:57:46 +0100

    218: introduces POM for e2e tests

--- system-test/e2e/e2eFeatureTests.cy.js
+++ system-test/e2e/e2eFeatureTests.cy.js
@@ -4,17 +4,17 @@ const BACKEND_URL = "http://script-manager-backend-uat.us-east-1.elasticbeanstal
 
 describe('E2E flow feature tests', () => {
   it('Add a new script', () => {
-    const title = "E2E test script name";
+    const title = "E2E test script name POM Model";
     const description = "E2E test script description";
     const code = "E2E test script code";
-
     cy.visit(FRONTEND_URL).title('Script Manager');
-    cy.contains('button','New Script').click();
-    cy.get('input[placeholder="Script name"]').type(title);
-    cy.get('input[placeholder="Script description"]').type(description);
-    cy.get('textarea[placeholder="Write your script here.."]').type(code);
-    cy.contains('button','Save Script').click();
-    cy.request(BACKEND_URL);
+    const scriptManagerPOM  = new ScriptManagerPOM();
+    scriptManagerPOM.initiateAddingNewScript();
+    scriptManagerPOM.typeInTitle(title);
+    scriptManagerPOM.typeInDescription(description);
+    scriptManagerPOM.typeInCode(code);
+    scriptManagerPOM.saveScript();
+    cy.wait(1000);
     cy.request(BACKEND_URL).then(
      (response) => {
        const obj = response.body.find(x => x.title === title);
@@ -26,19 +26,20 @@ describe('E2E flow feature tests', () => {
   });
 
   it('Edit first script in the list', () => {
-    const titleEdited = "E2E test script name edited";
+    const titleEdited = "E2E test script name edited POM Model 2";
     const descEdited = "E2E test script description edited";
     const codeEdited = "E2E test script code edited";
-
+    const scriptManagerPOM  = new ScriptManagerPOM();
     cy.visit(FRONTEND_URL).title('Script Manager');
-    cy.contains('button','Show History').click();
+    scriptManagerPOM.openHistory();
     cy.wait(1000);
-    cy.get('[data-testid^="checkbox-test-id"]').eq(0).check({ force: true });
-    cy.get('[data-testid^=edit-icon-test-id]').click();
-    cy.get('input[placeholder="Script name"]').clear().type(titleEdited);
-    cy.get('input[placeholder="Script description"]').clear().type(descEdited);
-    cy.get('textarea[placeholder="Write your script here.."]').clear().type(codeEdited);
-    cy.contains('button','Save Script').click();
+    scriptManagerPOM.selectFirstScript();
+    scriptManagerPOM.startEditingSelectedScript();
+    scriptManagerPOM.clearAndTypeInTitle(titleEdited);
+    scriptManagerPOM.clearAndTypeInDescription(descEdited);
+    scriptManagerPOM.clearAndTypeInCode(codeEdited);
+    scriptManagerPOM.saveScript();
+    cy.wait(1000);
     cy.request(BACKEND_URL).then(
       (response) => {
         const obj = response.body.find(x => x.title === titleEdited);
@@ -49,3 +50,42 @@ describe('E2E flow feature tests', () => {
     )
   });
 });
+
+class ScriptManagerPOM {
+  initiateAddingNewScript() {
+    cy.contains('button','New Script').click();
+  }
+  typeInTitle(title){
+    cy.get('input[placeholder="Script name"]').type(title);
+  }
+
+  typeInDescription(description){
+    cy.get('input[placeholder="Script description"]').type(description);
+  }
+  typeInCode(code){
+    cy.get('textarea[placeholder="Write your script here.."]').type(code);
+  }
+  saveScript() {
+    cy.contains('button','Save Script').click();
+  }
+
+  openHistory() {
+    cy.contains('button','Show History').click();
+  }
+
+  selectFirstScript() {
+    cy.get('[data-testid^="checkbox-test-id"]').eq(0).check({ force: true });
+  }
+  startEditingSelectedScript() {
+    cy.get('[data-testid^=edit-icon-test-id]').click();
+  }
+  clearAndTypeInTitle(titleEdited){
+    cy.get('input[placeholder="Script name"]').clear().type(titleEdited);
+  }
+  clearAndTypeInDescription(descEdited){
+    cy.get('input[placeholder="Script description"]').clear().type(descEdited);
+  }
+  clearAndTypeInCode(codeEdited){
+    cy.get('textarea[placeholder="Write your script here.."]').clear().type(codeEdited);
+  }
+}
